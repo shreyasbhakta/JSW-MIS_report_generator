41bae7d202e3c6302d46fabf8a679349b89949b0
shreyasbhakta <shreyasbhakta@gmail.com>
2021-03-18 00:35:01 +0530

    Splash, login screen,signup screen

---
+++ app/src/main/java/com/example/jsw_mis_report_generator/ViewPagerFragment.kt
@@ -0,0 +1,35 @@
+package com.example.jsw_mis_report_generator
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_view_pager.view.*
+
+class ViewPagerFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_view_pager, container, false)
+
+        val fragmentList = arrayListOf<Fragment>(
+            FirstScreen(),
+            SecondScreen(),
+            ThirdScreen()
+        )
+
+        val adapter = ViewPagerAdapter(
+            fragmentList,
+            requireActivity().supportFragmentManager,
+            lifecycle
+        )
+
+        view.viewPager.adapter = adapter
+
+        return view
+    }
+}
