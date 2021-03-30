dea9b0949a481f365a3d9b616f4e973fe4770934
Pallavi-M-S <65030860+Pallavi-M-S@users.noreply.github.com>
2021-03-31 02:59:03 +0530

    onboarding,inputforms,etc

---
+++ OnboardingScreen/app/src/main/java/com/example/onboardingscreen/OnboardingItem.kt
@@ -0,0 +1,9 @@
+package com.example.onboardingscreen
+
+import android.icu.text.CaseMap
+
+data class OnboardingItem (
+    val onboardingImage: Int,
+    val title: String,
+    val description: String
+)
