da946d6512a622700953fc94e7dfc91f8b2e8651
pallavimaralla <pallavimaralla@gmail.com>
2021-05-05 19:38:31 +0530

    CustomerDetailsOutput

---
+++ CustomerDetailsOutput/app/src/main/java/com/example/customerdisplay/Customers.kt
@@ -0,0 +1,6 @@
+package com.example.customerdisplay
+
+data class Customers(var cusEnum: String ?= null, var cusName: String ?= null, var ordernoc: String ?= null,
+                     var orderquan: String ?= null,var ordershipdate: String ?= null, var proForm: String ?= null,
+                     var proThickness: String ?= null, var proWidth: String ?= null, var prolength: String ?= null, var steelGrade: String ?= null)
+
