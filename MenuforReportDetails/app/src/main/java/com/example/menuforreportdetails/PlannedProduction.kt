3f0eef5c520edb9277f94a141e326a66b0af6a2c
pallavimaralla <pallavimaralla@gmail.com>
2021-05-14 23:04:41 +0530

    Menu for Report Details

---
+++ MenuforReportDetails/app/src/main/java/com/example/menuforreportdetails/PlannedProduction.kt
@@ -0,0 +1,11 @@
+package com.example.menuforreportdetails
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlannedProduction : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_planned_production)
+    }
+}
